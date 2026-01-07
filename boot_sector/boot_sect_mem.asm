b9947c9ca154e298fc417a7ce86e2c36c713cea9
Timon <timondaniels93@gmail.com>
2026-01-07 15:48:21 +0100

    until excersize 10

--- boot_sector/boot_sect_mem.asm
+++ boot_sector/boot_sect_mem.asm
@@ -5,19 +5,19 @@ int 0x10
 mov al, secret_key
 int 0x10
 
-mov al, "1"
+mov al, "2"
 int 0x10
 mov al, [secret_key]
 int 0x10
 
-mov al, "1"
+mov al, "3"
 int 0x10
 mov al, secret_key + 0x7C00
 int 0x10
 
-mov al, "1"
+mov al, "4"
 int 0x10
-mov al, 25 + 0x7C00
+mov al, [0x26 + 0x7C00]
 int 0x10
 
 jmp $
